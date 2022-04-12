e8508dcbf81829b60594a62699fff185fb4d5ec6
Learningcodes2021 <91432796+Learningcodes2021@users.noreply.github.com>
2022-04-12 16:36:20 -0400

    Refactored code

--- src/BackgroundVideo.js
+++ src/BackgroundVideo.js
@@ -2,12 +2,16 @@ import React from "react";
 import BackgroundVideo from "./images/sunset.mp4";
 import "./BackgroundVideo.css";
 
-export default function BackgroundVideo() {
-  return (
-    <div className="BackgroundVideo">
-      <video autoplay loop muted>
-        <source src="video.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );
+export default function BackgroundVideo(props) {
+  if (props.backgroundvideo.video >= 0) {
+    return null;
+  } else {
+    return (
+      <div className="BackgroundVideo">
+        <video autoplay loop muted>
+          <source src={props.backgroundvideo.video} type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
 }
